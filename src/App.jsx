fd20452362770923a899fd8f1c43942cd42af4d1
edwinbravo34 <edwinbravo2121@gmail.com>
2025-05-24 18:45:20 -0500

    agregar todos los edificios

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,125 @@ function App() {
   const [selectedRoom, setSelectedRoom] = useState('');
   const navigate = useNavigate();
 
+  const buildings = {
+    "Educación Continua": [
+      { code: "EC-2.1", name: "Salón EC-2.1" },
+      { code: "EC-2.2", name: "Salón EC-2.2" },
+      { code: "EC-2.3", name: "Salón EC-2.3" },
+      { code: "EC-2.4", name: "Salón EC-2.4" },
+      { code: "EC-3.1", name: "Salón EC-3.1" },
+      { code: "EC-3.2", name: "Salón EC-3.2" },
+      { code: "EC-3.3", name: "Salón EC-3.3" },
+      { code: "EC-3.5", name: "Salón EC-3.5" },
+      { code: "EC-A1-A2", name: "Salón EC-A1-A2" }
+    ],
+    "Almendros": [
+      { code: "ALM_3.1", name: "Salón ALM_3.1" },
+      { code: "ALM_3.10", name: "Salón ALM_3.10" },
+      { code: "ALM_3.11", name: "Salón ALM_3.11" },
+      { code: "ALM_3.2", name: "Salón ALM_3.2" },
+      { code: "ALM_3.3", name: "Salón ALM_3.3" },
+      { code: "ALM_3.4", name: "Salón ALM_3.4" },
+      { code: "ALM_3.5", name: "Salón ALM_3.5" },
+      { code: "ALM_3.6", name: "Salón ALM_3.6" },
+      { code: "ALM_3.7", name: "Salón ALM_3.7" },
+      { code: "ALM_3.8", name: "Salón ALM_3.8" },
+      { code: "ALM_3.9", name: "Salón ALM_3.9" },
+      { code: "ALM-1.1", name: "Salón ALM-1.1" },
+      { code: "ALM-1.2", name: "Salón ALM-1.2" },
+      { code: "ALM-1.3", name: "Salón ALM-1.3" },
+      { code: "ALM-1.4", name: "Salón ALM-1.4" },
+      { code: "ALM-1.5", name: "Salón ALM-1.5" },
+      { code: "ALM-1.6", name: "Salón ALM-1.6" },
+      { code: "ALM-1.7", name: "Salón ALM-1.7" },
+      { code: "ALM-1.8", name: "Salón ALM-1.8" },
+      { code: "AUD_ALM.", name: "Auditorio Almendros" }
+    ],
+    "Cedro Rosado": [
+      { code: "CR-2.1", name: "Salón CR-2.1" },
+      { code: "CR-3.1", name: "Salón CR-3.1" },
+      { code: "CR-3.2", name: "Salón CR-3.2" },
+      { code: "CR-3.3", name: "Salón CR-3.3" },
+      { code: "CR-3.4", name: "Salón CR-3.4" },
+      { code: "CR-4.1", name: "Salón CR-4.1" },
+      { code: "CR-4.2", name: "Salón CR-4.2" },
+      { code: "CR-4.3", name: "Salón CR-4.3" },
+      { code: "CR-4.4", name: "Salón CR-4.4" },
+      { code: "CR-5.1", name: "Salón CR-5.1" },
+      { code: "CR-5.2", name: "Salón CR-5.2" },
+      { code: "CR-5.3", name: "Salón CR-5.3" },
+      { code: "CR-5.4", name: "Salón CR-5.4" },
+      { code: "CR-6.2", name: "Salón CR-6.2" },
+      { code: "CR-6.3", name: "Salón CR-6.3" },
+      { code: "CR-AU.1", name: "Auditorio Cedro Rosado 1" },
+      { code: "CR-AU.2", name: "Auditorio Cedro Rosado 2" },
+      { code: "CR-AU.3", name: "Auditorio Cedro Rosado 3" }
+    ],
+    "Guayacanes": [
+      { code: "EG-2.4", name: "Salón EG-2.4" },
+      { code: "EG-3.1", name: "Salón EG-3.1" },
+      { code: "EG-4.1", name: "Salón EG-4.1" },
+      { code: "EG-5.2", name: "Salón EG-5.2" }
+    ],
+    "Guaduales": [
+      { code: "GUADUALES1", name: "Salón GUADUALES1" },
+      { code: "GUADUALES2", name: "Salón GUADUALES2" }
+    ],
+    "Lago": [
+      { code: "LG-1.0", name: "Salón LG-1.0" },
+      { code: "LG-1.2", name: "Salón LG-1.2" },
+      { code: "LG-1.3", name: "Salón LG-1.3" },
+      { code: "LG-1.4", name: "Salón LG-1.4" },
+      { code: "LG-1.5", name: "Salón LG-1.5" },
+      { code: "LG-1.6", name: "Salón LG-1.6" },
+      { code: "LG-1.7", name: "Salón LG-1.7" },
+      { code: "LG-1.8", name: "Salón LG-1.8" },
+      { code: "LG-2.0", name: "Salón LG-2.0" },
+      { code: "LG-2.12", name: "Salón LG-2.12" },
+      { code: "LG-2.13", name: "Salón LG-2.13" },
+      { code: "LG-2.14", name: "Salón LG-2.14" },
+      { code: "LG-2.2", name: "Salón LG-2.2" },
+      { code: "LG-2.6", name: "Salón LG-2.6" },
+      { code: "LG-2.8", name: "Salón LG-2.8" },
+      { code: "LG-2.9", name: "Salón LG-2.9" },
+      { code: "LG-3.1", name: "Salón LG-3.1" },
+      { code: "LG-3.3", name: "Salón LG-3.3" },
+      { code: "LG-3.4", name: "Salón LG-3.4" },
+      { code: "LG-3.5", name: "Salón LG-3.5" },
+      { code: "LG-4.0", name: "Salón LG-4.0" },
+      { code: "LG-4.2", name: "Salón LG-4.2" },
+      { code: "LG-4.3", name: "Salón LG-4.3" },
+      { code: "LG-4.4", name: "Salón LG-4.4" },
+      { code: "LG-4.6", name: "Salón LG-4.6" }
+    ],
+    "Palmas": [
+      { code: "PL-1.3", name: "Salón PL-1.3" },
+      { code: "PL-1.4", name: "Salón PL-1.4" },
+      { code: "PL-1.5", name: "Salón PL-1.5" },
+      { code: "PL-1.6", name: "Salón PL-1.6" }
+    ],
+    "Samán": [
+      { code: "SM-1.3", name: "Salón SM-1.3" },
+      { code: "SM-1.4", name: "Salón SM-1.4" },
+      { code: "SM-1.5", name: "Salón SM-1.5" },
+      { code: "SM-1.6", name: "Salón SM-1.6" },
+      { code: "SM-1.7", name: "Salón SM-1.7" },
+      { code: "SM-1.8", name: "Salón SM-1.8" },
+      { code: "SM-1.9", name: "Salón SM-1.9" },
+      { code: "SM-2.2", name: "Salón SM-2.2" },
+      { code: "SM-2.3", name: "Salón SM-2.3" },
+      { code: "SM-2.4", name: "Salón SM-2.4" },
+      { code: "SM-2.5", name: "Salón SM-2.5" },
+      { code: "SM-2.6", name: "Salón SM-2.6" },
+      { code: "SM-2.7", name: "Salón SM-2.7" },
+      { code: "SM-2.8", name: "Salón SM-2.8" },
+      { code: "SM-2.9", name: "Salón SM-2.9" },
+      { code: "SM-2.10", name: "Salón SM-2.10" },
+      { code: "SM-2.11", name: "Salón SM-2.11" },
+      { code: "SM-2.12", name: "Salón SM-2.12" }
+    ]
+  };
+
   const handleContinue = () => {
     if (selectedRoom) {
       navigate(`/salon/${selectedRoom}`);
@@ -23,23 +142,26 @@ function App() {
         <label htmlFor="room" className="block text-lg font-semibold mb-2 text-gray-700">
           Seleccione el salón
         </label>
-        <select
-          id="room"
-          value={selectedRoom}
-          onChange={(e) => setSelectedRoom(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded"
-        >
-          <option value="">-- Seleccione una opción --</option>
-          <option value="EC-2.1">EC-2.1</option>
-          <option value="EC-2.2">EC-2.2</option>
-          <option value="EC-2.3">EC-2.3</option>
-          <option value="EC-2.4">EC-2.4</option>
-          <option value="EC-3.1">EC-3.1</option>
-          <option value="EC-3.2">EC-3.2</option>
-          <option value="EC-3.3">EC-3.3</option>
-          <option value="EC-3.5">EC-3.5</option>
-          <option value="EC-A1-A2">EC-A1-A2</option>
-        </select>
+
+        <div className="space-y-4">
+          {Object.entries(buildings).map(([buildingName, rooms]) => (
+            <div key={buildingName} className="border rounded-lg p-3">
+              <h3 className="font-semibold text-lg mb-2">{buildingName}</h3>
+              <select
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e) => setSelectedRoom(e.target.value)}
+                value={selectedRoom}
+              >
+                <option value="">-- Seleccione un salón --</option>
+                {rooms.map((room) => (
+                  <option key={room.code} value={room.code}>
+                    {room.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ))}
+        </div>
 
         <button
           onClick={handleContinue}
--- src/components/SalonInfo.jsx
+++ src/components/SalonInfo.jsx
@@ -9,6 +9,7 @@ function SalonInfo() {
   const [error, setError] = useState(null);
 
   const roomImages = {
+    // Educación Continua
     "EC-2.1": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
     "EC-2.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
     "EC-2.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
@@ -17,7 +18,112 @@ function SalonInfo() {
     "EC-3.2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
     "EC-3.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
     "EC-3.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
-    "EC-A1-A2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg"
+    "EC-A1-A2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+
+    // Almendros
+    "ALM_3.1": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "ALM_3.10": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "ALM_3.11": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.11.jpg",
+    "ALM_3.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.2.jpg",
+    "ALM_3.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.3.jpg",
+    "ALM_3.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.4.jpg",
+    "ALM_3.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "ALM_3.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.2.jpg",
+    "ALM_3.7": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.5.jpg",
+    "ALM_3.8": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/3.8.jpg",
+    "ALM_3.9": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "ALM-1.1": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "ALM-1.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "ALM-1.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "ALM-1.4": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "ALM-1.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "ALM-1.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Almendros/1.6.jpg",
+    "ALM-1.7": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "ALM-1.8": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "AUD_ALM.": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+
+    // Cedro Rosado
+    "CR-2.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+3.1+-+2.jpg",
+    "CR-3.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+3.1.jpg",
+    "CR-3.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "CR-3.3": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "CR-3.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "CR-4.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "CR-4.2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "CR-4.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "CR-4.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "CR-5.1": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "CR-5.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+5.3+-+2.jpg",
+    "CR-5.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+5.3.jpg",
+    "CR-5.4": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "CR-6.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/Cr+6.2.jpg",
+    "CR-6.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/Cr+6.3.jpg",
+    "CR-AU.1": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "CR-AU.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+Aud+2.jpg",
+    "CR-AU.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Cedro+rosado/CR+Aud+3.jpg",
+
+    // Guayacanes
+    "EG-2.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/EG+3.1.jpg",
+    "EG-3.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/EG+3.1.jpg",
+    "EG-4.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/EG+4.1.jpg",
+    "EG-5.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/EG+5.2.jpg",
+
+    // Guaduales
+    "GUADUALES1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/Guaduales+2.jpg",
+    "GUADUALES2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/Guaduales+2.jpg",
+
+    // Lago
+    "LG-1.0": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+0.3.jpg",
+    "LG-1.2": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+1.1.jpg",
+    "LG-1.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "LG-1.4": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "LG-1.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "LG-1.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "LG-1.7": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "LG-1.8": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "LG-2.0": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.3.jpg",
+    "LG-2.12": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.1.jpg",
+    "LG-2.13": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.4.jpg",
+    "LG-2.14": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.6.jpg",
+    "LG-2.2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "LG-2.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.7.jpg",
+    "LG-2.8": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.10.jpg",
+    "LG-2.9": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Lagos/LG+2.11.jpg",
+    "LG-3.1": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "LG-3.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "LG-3.4": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "LG-3.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "LG-4.0": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "LG-4.2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "LG-4.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "LG-4.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "LG-4.6": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+
+    // Palmas
+    "PL-1.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/PL-+S.3.JPG",
+    "PL-1.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/PL-+S.4.png",
+    "PL-1.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/PL-+S.5.JPG",
+    "PL-1.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/Guadual-Guayacanes-Palmas/PL-+S.6.JPG",
+
+    // Samán
+    "SM-1.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-1.4": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-1.5": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "SM-1.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-1.7": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-1.8": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "SM-1.9": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-2.2": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-2.3": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "SM-2.4": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-2.5": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-2.6": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "SM-2.7": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-2.8": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-2.9": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg",
+    "SM-2.10": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-3.5.jpg",
+    "SM-2.11": "https://www.yarquitectura.com/wp-content/uploads/2023/06/auditorios-arquitectura.jpg",
+    "SM-2.12": "https://noutube-aplicaciones-moviles.s3.us-east-2.amazonaws.com/EC-+3.3.jpg"
   };
 
   useEffect(() => {
